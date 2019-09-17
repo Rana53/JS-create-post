db74303e67a8e712ffc4547d7a686ccb6d906b29
Rana53 <ranajahid.cseru@gmail.com>
2019-09-17 20:23:03 +0600

    an interface for post was created and update with it's required file

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from "@angular/core";
+import { Post } from '../post.model';
 
 @Component({
     selector: 'app-post-create',
@@ -9,9 +10,9 @@ import { Component, EventEmitter, Output } from "@angular/core";
 export class PostCreateComponent{
     postTitle = "";
     postContent = "";
-    @Output() postCreated = new EventEmitter();
+    @Output() postCreated = new EventEmitter<Post>();
     clickOnButton(){
-        const post = {
+        const post: Post = {
             title: this.postTitle,
             content: this.postContent
         }
