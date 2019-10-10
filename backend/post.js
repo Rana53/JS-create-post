5b838a428368db9e3a046c76fd66adf26cb1e926
Rana53 <ranajahid.cseru@gmail.com>
2019-10-10 17:20:01 +0600

    post model created

--- backend/post.js
+++ backend/post.js
@@ -4,5 +4,4 @@ const postSchema = new mongoose.Schema({
     title: {type: String, required: true},
     content: {type: String, required: true}
 });
-
 module.exports = mongoose.model('Post', postSchema);
