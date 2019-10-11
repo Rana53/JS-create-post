5a4fc224db00a6802907eb2f458713d719b84237
Rana53 <ranajahid.cseru@gmail.com>
2019-10-11 20:37:49 +0600

    sucessfully added a post in database

--- backend/app.js
+++ backend/app.js
@@ -40,21 +40,17 @@ app.post("/api/posts", (req, res, next) => {
     });
 });
 app.get("/api/posts", (req, res, next) => {
-    const posts = [{
-            id: "5whefdjhjadslfj",
-            title: "First server-side post",
-            content: "This is comming from server"
-        },
-        {
-            id: "764wgkdajgfdwg",
-            title: "Second server-side post",
-            content: 'This is also comming from server'
-        }
-    ];
-    res.status(200).json({
+    Post.find()
+    .then((doc) =>{
+      res.status(200).json({
         message: 'Post fatched successfully',
-        posts: posts
+        posts: doc
+      });
+    })
+    .catch(()=>{
+        console.log('Error occure to retrieve post data');
     });
+    
 });
 
 
