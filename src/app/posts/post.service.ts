5283db5c1f9686b259cbd85e8798fba6b76b3daa
Rana53 <ranajahid.cseru@gmail.com>
2019-10-15 01:15:29 +0600

    from backend post _id mapped into post id

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Post} from '../posts/post.model';
 import { HttpClient} from "@angular/common/http";
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { post } from 'selenium-webdriver/http';
 
 @Injectable({providedIn:'root'})
 export class PostService {
@@ -21,9 +23,18 @@ export class PostService {
         return this.postUpdated.asObservable();
     }
     getPosts(){
-        this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-          .subscribe((postData) => {
-              this.allPosts = postData.posts;   
+        this.http.get<{message: string, posts: any}>('http://localhost:3000/api/posts')
+          .pipe(map((postData) =>{
+              return postData.posts.map((post) => {
+                  return {
+                      title: post.title,
+                      content: post.content,
+                      id: post._id
+                  }
+              });
+          }))
+          .subscribe((transformedData) => {
+              this.allPosts = transformedData;   
               this.postUpdated.next([...this.allPosts]);
           });
         //return [...this.allPosts];
