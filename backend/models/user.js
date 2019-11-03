51341b2f587b8043a90162c7326325683a2e7e3d
Rana53 <ranajahid.cseru@gmail.com>
2019-11-03 23:33:18 +0600

    created a user model for users and install mongoose-unique-validator for unique user

---
+++ backend/models/user.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true},
+    password: { type: String, required: true}
+});
+module.exports = mongoose.model('User', userSchema);
